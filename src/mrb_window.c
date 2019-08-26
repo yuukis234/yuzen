72ba3e9a058ec6235facea4375c1c5f75bb71b4d
yuuki <kumagai.robo@gmail.com>
2019-08-26 20:50:19 +0900

    マージ

--- src/mrb_window.c
+++ src/mrb_window.c
@@ -9,6 +9,7 @@
 #include "mruby.h"
 #include "mruby/data.h"
 #include "mruby/variable.h"
+#include "mruby/string.h"
 #include "mrb_games.h"
 #include <GLFW/glfw3.h>
 
@@ -60,6 +61,7 @@ static mrb_value mrb_games_view(mrb_state *mrb, mrb_value self)
 {
   GLFWwindow* window;
 
+<<<<<<< current
   /* Initialize the library */
   /* ここでゲーム自体を初期化します。ここでイニシャライズできなければnilを返します。*/
   if (!glfwInit())
@@ -116,11 +118,67 @@ static mrb_value mrb_games_view(mrb_state *mrb, mrb_value self)
   /* ここでゲーム自体を終了させます。*/
   glfwTerminate();
   return mrb_nil_value();
+=======
+    /* Initialize the library */
+    /* ここでゲーム自体を初期化します。ここでイニシャライズできなければnilを返します。*/
+    if (!glfwInit())
+        return mrb_nil_value();
+
+    /* Create a windowed mode window and its OpenGL context */
+    /* ここで画面を設定します。*/
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return mrb_nil_value();
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    /* ここでメインロジックをループさせます。*/
+    /* TODO:このループをループクラスとして作る*/
+    while (!glfwWindowShouldClose(window))
+    {
+        /*背景の色を変更する*/
+        glClearColor(0.6, 0.8, 1.0, 1.0);
+        /* Render here */
+        /*バッファを初期化するカラー情報を設定*/
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        /* windowを入れ替える。 */
+        glfwSwapBuffers(window);
+
+        /*図形の色を変えます。*/
+        glColor4f(1.0, 0.0, 0.0, 1.0);
+
+        /*glBeginで図形を初期化*/
+        glBegin(GL_TRIANGLES);
+        /*glVertex2fで頂点を指定*/
+        glVertex2f(   0,  0.5);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f( 0.5, -0.5);
+        glEnd();
+
+        /* windowをbuffewから交換します */
+        glfwSwapBuffers(window);
+        /* Poll for and process events */
+        /* マウスの操作などのイベントを取り出し、それを記録します。*/
+        /* ここを消すと描画がめちゃくちゃ早いポケモンフラッシュになる。*/
+        glfwPollEvents();
+    }
+
+    /* ここでゲーム自体を終了させます。*/
+    glfwTerminate();
+    return mrb_nil_value();
+>>>>>>> before discard
 }
 
-mrb_value bar_method(mrb_state* mrb, mrb_value self){
-  puts("Executing example command!");
-  return mrb_attr_get(mrb, self, mrb_intern_str(mrb, mrb_str_new_lit(mrb, "data" )));
+static mrb_value bar_method(mrb_state* mrb, mrb_value self){
+  mrb_games_data *h = DATA_PTR(self);
+  return mrb_ptr_to_str(mrb, h->str);
 }
 
 void mrb_window_init(mrb_state *mrb)
